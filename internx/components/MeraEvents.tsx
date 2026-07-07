843568600097c133a7a71c93f96599c6cfd85282
Akhil Raj <akhilrajuvysyaraju19.com>
2026-07-07 10:21:38 +0530

    chnage the event link of the meraevents registation link

--- internx/components/MeraEvents.tsx
+++ internx/components/MeraEvents.tsx
@@ -2,10 +2,11 @@
 import React from 'react'
 
 const MeraEvents = () => {
+  const Link:string = "https://www.meraevents.com/ticketWidget?eventId=270783&ucode=organizer&wcode=9063CD-9063CD-333333-9063CD-&theme=1&samepage=1"
   return (
     <div>
         <button
-              onClick={() => { window.open("https://www.meraevents.com/ticketWidget?eventId=270398&ucode=organizer&wcode=9063CD-9063CD-333333-9063CD-&theme=1&samepage=1", "_blank") }}
+              onClick={() => { window.open(Link, "_blank") }}
               className="bg-[#ff1a1a] hover:bg-[#ff3333] transition-colors text-white rounded-full font-medium hover:cursor-pointer"
               style={{ fontSize: 'clamp(0.85rem, 2.5vw, 0.95rem)', padding: 'clamp(0.55rem, 1.5vw, 0.875rem) clamp(1.5rem, 4vw, 2.5rem)' }}
             >
